af0381fff56dfa64d46b769a95c43433bac2f270
SHIVAM RAI <preetisahani987@gmail.comm>
2023-01-30 15:59:52 +0530

    commit

---
+++ conn.js
@@ -0,0 +1,19 @@
+// connecting mongodb with node 
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://0.0.0.0:27017';
+const database = 'e-comm';
+const client = new MongoClient(url);
+
+async function getData(){
+
+    let result = await client.connect();
+    let db = result.db(database);
+    let collection = db.collection('products');
+    let response = await collection.find().toArray();
+    console.log(response[0]);
+
+
+}
+
+getData();
