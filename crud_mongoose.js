65a4a6f9f635fdd5f2323966371fc391f7408c40
SHIVAM RAI <preetisahani987@gmail.comm>
2023-02-01 16:46:17 +0530

    commit

---
+++ crud_mongoose.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+require('./congig');
+const Products = require('./product');
+
+
+app.use(express.json());
+
+app.post('/create',async (req,res)=>{
+    let data = new Products(req.body);
+    let result = await data.save();
+    console.log(result)
+    res.send(result);
+});
+
+app.get('/list',async (req,res)=>{
+    let data = await Products.find();
+    res.send(data);
+});
+
+app.delete('/delete/:_id',async (req,res)=>{
+    let data = await Products.deleteOne(req.params);
+    res.send(data);
+});
+
+app.put('/update/:_id',async (req,res)=>{
+    let data = await Products.updateOne(
+        req.params,
+        {
+           $set:req.body
+        }
+        );
+    res.send(data);
+});
+
+app.listen(5000);
